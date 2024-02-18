662b70c65e0e56c00474fb216993f4bc9a1eda35
Theosdad <Obie_1@mail.ru>
2024-02-18 23:09:22 +0300

    fix

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -10,14 +10,21 @@ function Movie(props) {
     return (
         <div id={id} className="card movie">
             <div className="card-image waves-effect waves-block waves-light">
-                { 
-                    poster === 'N/A' ? 
-                    <img className="activator movie-image" alt="" src={`https://placehold.co/600x400?text=${title}`} /> 
-                    : <img className="activator movie-image" alt="" src={poster} /> 
+                {
+                    poster === 'N/A' ?
+                        <img className="activator movie-image" alt="" src={`https://placehold.co/600x400?text=${title}`} />
+                        : <img className="activator movie-image" alt="" src={poster} />
                 }
             </div>
             <div className="card-content">
-                <a className="card-title activator grey-text text-darken-4" href={`https://www.imdb.com/title/${id}/`}>{title}</a>
+                <a
+                    className="card-title activator grey-text text-darken-4"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`https://www.imdb.com/title/${id}/`}
+                >
+                    {title}
+                </a>
                 <p>{year}
                     <span className="right">{type}</span>
                 </p>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="nav-wrapper">
             <a href="#!" className="brand-logo">
                 <img src={logo} width="50" alt="" />
-                <span>Movie App</span>
+                <span>Movie&nbsp;App</span>
             </a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="#!">Repo</a></li>
